7f8dd2d8beaa63c03540d5331e2c8dedca3a2b0d
David Alves Dutra <dadutra@hotmail.com>
2025-01-27 09:32:39 -0300

    saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist ok.

--- src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,14 +1,18 @@
 package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
 
 public class ScoreControllerRA {
 
@@ -41,7 +45,23 @@ public class ScoreControllerRA {
 	}
 	
 	@Test
-	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
+	@DisplayName("save Score Should Return Not Found When Movie Id Does Not Exist")
+	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+		postScore.put("movieId", "999");
+		JSONObject addScore = new JSONObject(postScore);
+
+		given()
+				.header("Content-type", "application-json")
+				.header("Authorization", "Bearer " + adminToken)
+				.body(addScore)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+
+				.when()
+				.put("/scores")
+
+				.then()
+				.statusCode(404);
 	}
 	
 	@Test
