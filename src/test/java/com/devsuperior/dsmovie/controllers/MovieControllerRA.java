2456d40db195ec20e3e5dfeffcad780b4bdb5a5e
David Alves Dutra <dadutra@hotmail.com>
2025-01-27 08:50:58 -0300

    Preparação. BeforeEach ok.

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,9 +1,38 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
 public class MovieControllerRA {
+
+	private String clientUsername, clientPassword,
+			adminUsername, adminPassword, clientToken, adminToken;
+
+	private Long existingId, nonExistingId;
+
+	@BeforeEach
+	public void setUp() throws JSONException {
+		baseURI = "http://localhost:8080";
+
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
+
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+
+		existingId = 1L;
+		nonExistingId = 999L;
+	}
 	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
